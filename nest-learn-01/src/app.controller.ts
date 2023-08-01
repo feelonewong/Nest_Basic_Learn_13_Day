3256b5fd0554d89228f4429b037e9f2838b8f313
feelonewong <feelonewong@gmail.com>
2023-08-01 16:31:40 +0800

    update:第1章nest概念结束

--- nest-learn-01/src/app.controller.ts
+++ nest-learn-01/src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get } from '@nestjs/common'
+import { AppService } from './app.service'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {
+    // this.appService = new AppService()
+  }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  findOne(): string {
+    return this.appService.findOne()
   }
 }
